3b3b8435802c2441d7a656d60617ba180016b752
Félix Martins <felix.marcial@hotmail.com>
2023-11-22 17:00:35 +0000

    Fixed profile page includes and profile link

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <main id="postpage" class="grid grid-cols-4 absolute top-32 left-0 w-screen px-64">
     @include('partials.side.navbar')
-    <section class="col-span-2 flex flex-grow pt-16 overflow-y-auto scrollbar-hide" id="content">    
+    <section class="col-span-2 flex flex-grow pt-16 overflow-y-auto scrollbar-hide" id="content">
         @include('partials.post', ['post' => $post, 'displayComments' => true])
     </section>
     @include('partials.side.notifications-tab')
--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -3,13 +3,13 @@
 @section('title', 'profile-page')
 
 <main id="homepage" class="grid grid-cols-4 absolute top-32 left-0 w-screen px-64">
-    @include('partials.side.left-tab')
+    @include('partials.side.navbar')
     <section class="col-span-2 flex flex-grow pt-16 overflow-y-auto scrollbar-hide " id="content">
         <section class="user-banner flex-grow h-full">
             @include('partials.profile.user-banner')
             @include('partials.profile.post-profile')
         </section>
     </section>
-    @include('partials.side.right-tab')
+    @include('partials.side.right-tab-profile')
 </main>
 
