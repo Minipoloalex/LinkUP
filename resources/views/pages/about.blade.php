0abd881afa662f7c3d9751f2a21bc480ed15abc8
Domingos Santos <domingosjmsantos@gmail.com>
2023-11-26 16:31:57 +0000

    about us done

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,25 +3,24 @@
 @section('title', 'About')
 
 @section('content')
-<main id="aboutuspage" class="grid grid-cols-4 absolute top-32 left-0 w-screen px-64">
+<main id="aboutUs" class="grid grid-cols-4 absolute top-32 left-0 w-screen px-64">
     @include('partials.side.navbar')
-    <section class="col-span-2 flex flex-grow pt-16 overflow-y-auto scrollbar-hide" id="content">
-        <section class="mb-8">
-            <h2 class="text-2xl font-bold mb-4">Our Mission</h2>
-            <p class="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </section>
+    <section class="col-span-2 flex flex-col flex-grow pt-16 overflow-y-auto scrollbar-hide" id="content">
+        
+        <div class="flex flex-col items-center justify-center w-full h-full px-2">
+            <h1 class="text-4xl font-bold text-center">Meet the Team</h1>
+            <p class="text-center"> We are a group of students from FEUP, Portugal. We are currently enrolled in the course of LEIC</p>
+        </div>
 
-        <section class="mb-8">
-            <h2 class="text-2xl font-bold mb-4">Our Team</h2>
-            <p class="text-gray-700">Meet the passionate individuals who make our team strong and dedicated to our mission.</p>
-        </section>
+        <!-- create Contact Form here -->
+        <!-- like a report form -->
+        @include('partials.about_us_members')
 
-        <section>
-            <h2 class="text-2xl font-bold mb-4">Contact Us</h2>
-            <p class="text-gray-700">Feel free to reach out to us if you have any questions or inquiries.</p>
-            <p class="text-gray-700">Email: info@example.com</p>
-            <p class="text-gray-700">Phone: (123) 456-7890</p>
-        </section>
+        <div class="flex justify-center mt-4">
+            <a href="{{ url('/contact') }}" class="bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded-full">Contact Us</a>
+        </div>
+
+        
     </section>
     @include('partials.side.notifications-tab')
     <div id="dark-overlay" class="hidden fixed top-0 left-0 w-full h-full bg-black" style="opacity: 0.8;"></div>
---
+++ resources/views/partials/about_us_members.php
@@ -0,0 +1,42 @@
+<div class="w-full mt-4">
+    <div class="flex flex-wrap justify-center items-center">
+        <!-- First Row -->
+        <div class="flex w-full justify-center">
+            <div class="w-min-md:w-32 h-32 rounded-lg mr-2">
+                <div class="w-16 h-16 rounded-full bg-gray-300 mx-auto -mt-8">
+                    <img class="w-full h-full object-cover rounded-full" src="{{ url('images/creators/domingos.jpg') }}" alt="Profile image">
+                </div>
+                <div class="flex-grow mb-2">
+                    <p class="text-center font-semibold">Domingos Santos</p>
+                </div>
+            </div>
+            <div class="w-min-md:w-32 h-32 rounded-lg mr-2">
+                <div class="w-16 h-16 rounded-full bg-gray-300 mx-auto -mt-8">
+                    <img class="w-full h-full object-cover rounded-full" src="{{ url('images/creators/duarte.jpg') }}" alt="Profile image">
+                </div>
+                <div class="flex-grow mb-2">
+                    <p class="text-center font-semibold">Duarte Gonçalves</p>
+                </div>
+            </div>
+        </div>
+        <!-- Second Row -->
+        <div class="flex w-full justify-center">
+            <div class="w-min-md:w-32 h-32 rounded-lg mr-2">
+                <div class="w-16 h-16 rounded-full bg-gray-300 mx-auto -mt-8">
+                    <img class="w-full h-full object-cover rounded-full" src="{{ url('images/creators/marco.jpg') }}" alt="Profile image">
+                </div>
+                <div class="flex-grow mb-2">
+                    <p class="text-center font-semibold">Marco Vila Boas</p>
+                </div>
+            </div>
+            <div class="w-min-md:w-32 h-32 rounded-lg mr-2">
+                <div class="w-16 h-16 rounded-full bg-gray-300 mx-auto -mt-8">
+                    <img class="w-full h-full object-cover rounded-full" src="{{ url('images/creators/felix.jpg') }}" alt="Profile image">
+                </div>
+                <div class="flex-grow mb-2">
+                    <p class="text-center font-semibold">Félix Martins</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
