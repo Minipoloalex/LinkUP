70025e73da4346d7446efeaf128e0f93b1a9823b
Domingos Santos <domingosjmsantos@gmail.com>
2023-11-26 15:37:56 +0000

    now contact is done

--- public/js/contact.js
+++ public/js/contact.js
@@ -2,19 +2,26 @@ function submitForm() {
     const form = document.getElementById('contactFormElement');
     const contactForm = document.getElementById('contactForm');
   
-    // Hide the form and display the thank you message
-    form.style.display = 'none';
-    const thankYouMessage = document.createElement('p');
-    thankYouMessage.textContent = 'Thank you, your message was submitted';
-    thankYouMessage.style.fontSize = '1.2rem';
-    contactForm.appendChild(thankYouMessage);
-  
-    // Reset the form after 3 seconds and show it again
-    setTimeout(function() {
-      contactForm.removeChild(thankYouMessage);
-      form.style.display = 'block';
-      form.reset();
-    }, 3000);
+    if (form.checkValidity()) {
+        // Hide the form and display the thank you message
+        form.style.display = 'none';
+        const thankYouMessage = document.createElement('p');
+        thankYouMessage.textContent = 'Thank you, your message was submitted';
+        thankYouMessage.style.fontSize = '1.2rem';
+        contactForm.appendChild(thankYouMessage);
+    
+        // Reset the form after 3 seconds and show it again
+        setTimeout(function() {
+        contactForm.removeChild(thankYouMessage);
+        form.style.display = 'block';
+        form.reset();
+        }, 3000);
+
+    } else {
+        // Form is invalid, display error messages or handle accordingly
+        // You can add error messages or perform actions for invalid fields
+        console.log('Form is invalid!');
+    }
   }
   
   function cancelForm() {
@@ -25,4 +32,17 @@ function submitForm() {
     // Reload the page
     window.location.reload();
   }
+
   
+
+
+  
+/* make the message field automatically grow as the user types */
+let messageInput = document.getElementById('message');
+
+messageInput.addEventListener('input', function () {
+    this.style.height = 'auto'; // Reset the height to auto to ensure the correct height calculation
+    this.style.height = (this.scrollHeight) + 'px'; // Set the height based on content
+});
+
+
