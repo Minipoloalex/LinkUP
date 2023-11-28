893af18609692884a7f94586e1510778d7be32c2
Domingos Santos <domingosjmsantos@gmail.com>
2023-11-28 17:01:01 +0000

    fixed bugs

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -7,14 +7,16 @@
     @include('partials.side.navbar')
     <section class="col-span-2 flex flex-col flex-grow pt-16 overflow-y-auto scrollbar-hide" id="content">
         
-        <div class="flex flex-col items-center justify-center w-full h-full px-2">
+        <div class="flex flex-col items-center justify-center w-full h-full px-2 mb-4">
             <h1 class="text-4xl font-bold text-center">Meet the Team</h1>
             <p class="text-center"> We are a group of students from FEUP, Portugal. We are currently enrolled in the course of LEIC</p>
         </div>
 
+        @include('partials.about_us_members')
+
+
         <!-- create Contact Form here -->
         <!-- like a report form -->
-        @include('partials.about_us_members')
 
         <div class="flex justify-center mt-4">
             <a href="{{ url('/contact') }}" class="bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded-full">Contact Us</a>
--- resources/views/partials/about_us_members.blade.php
+++ resources/views/partials/about_us_members.blade.php
@@ -1,42 +1,43 @@
-<div class="w-full mt-4">
-    <div class="flex flex-wrap justify-center items-center">
-        <!-- First Row -->
-        <div class="flex w-full justify-center">
-            <div class="w-min-md:w-32 h-32 rounded-lg mr-2">
-                <div class="w-16 h-16 rounded-full bg-gray-300 mx-auto -mt-8">
-                    <img class="w-full h-full object-cover rounded-full" src="{{ url('images/creators/domingos.jpg') }}" alt="Profile image">
-                </div>
-                <div class="flex-grow mb-2">
-                    <p class="text-center font-semibold">Domingos Santos</p>
-                </div>
+
+<div class="flex flex-wrap items-center justify-center">
+    <!-- First Row -->
+    <div class="flex">
+        <div class="w-32 h-40 rounded-lg flex flex-col items-center justify-center">
+            <div class="w-20 h-20 rounded-full">
+                <img class="w-full h-full object-cover rounded-full" src="{{ url('images/creators/domingos.jpg') }}" alt="Profile image">
             </div>
-            <div class="w-min-md:w-32 h-32 rounded-lg mr-2">
-                <div class="w-16 h-16 rounded-full bg-gray-300 mx-auto -mt-8">
-                    <img class="w-full h-full object-cover rounded-full" src="{{ url('images/creators/duarte.jpg') }}" alt="Profile image">
-                </div>
-                <div class="flex-grow mb-2">
-                    <p class="text-center font-semibold">Duarte Gonçalves</p>
-                </div>
+            <div class="flex-grow">
+                <p class="text-center font-semibold">Domingos Santos</p>
             </div>
         </div>
-        <!-- Second Row -->
-        <div class="flex w-full justify-center">
-            <div class="w-min-md:w-32 h-32 rounded-lg mr-2">
-                <div class="w-16 h-16 rounded-full bg-gray-300 mx-auto -mt-8">
-                    <img class="w-full h-full object-cover rounded-full" src="{{ url('images/creators/marco.jpg') }}" alt="Profile image">
-                </div>
-                <div class="flex-grow mb-2">
-                    <p class="text-center font-semibold">Marco Vila Boas</p>
-                </div>
+        <div class="w-32 h-40 rounded-lg flex flex-col items-center justify-center">
+            <div class="w-20 h-20 rounded-full">
+                <img class="w-full h-full object-cover rounded-full" src="{{ url('images/creators/duarte.jpg') }}" alt="Profile image">
             </div>
-            <div class="w-min-md:w-32 h-32 rounded-lg mr-2">
-                <div class="w-16 h-16 rounded-full bg-gray-300 mx-auto -mt-8">
-                    <img class="w-full h-full object-cover rounded-full" src="{{ url('images/creators/felix.jpg') }}" alt="Profile image">
-                </div>
-                <div class="flex-grow mb-2">
-                    <p class="text-center font-semibold">Félix Martins</p>
-                </div>
+            <div class="flex-grow">
+                <p class="text-center font-semibold">Duarte Gonçalves</p>
+            </div>
+        </div>
+    </div>
+    <!-- Second Row -->
+    <div class="flex">
+        <div class="w-32 h-40 rounded-lg flex flex-col items-center justify-center">
+            <div class="w-20 h-20 rounded-full">
+                <img class="w-full h-full object-cover rounded-full" src="{{ url('images/creators/marco.jpg') }}" alt="Profile image">
+            </div>
+            <div class="flex-grow">
+                <p class="text-center font-semibold">Marco Vila Boas</p>
+            </div>
+        </div>
+        <div class="w-32 h-40 rounded-lg flex flex-col items-center justify-center">
+            <div class="w-20 h-20 rounded-full">
+                <img class="w-full h-full object-cover rounded-full" src="{{ url('images/creators/felix.jpg') }}" alt="Profile image">
+            </div>
+            <div class="flex-grow">
+                <p class="text-center font-semibold">Félix Martins</p>
             </div>
         </div>
     </div>
 </div>
+
+
