dc09afde8c64b9dcac5f3966f66dd39dab0a1807
Domingos Santos <domingosjmsantos@gmail.com>
2023-12-01 13:03:44 +0000

    missing unlike

--- public/js/like.js
+++ public/js/like.js
@@ -1,63 +1,94 @@
-// Event delegation for the like button inside the #timeline container
-document.querySelector('#timeline').addEventListener('click', async function(e) {
-  console.log(e.target);
-  if (e.target && e.target.matches('.like-button')) {
-      e.preventDefault();
-
-      const postId = e.target.getAttribute('data-id');
-      const likeButton = e.target; // Cache the like button element
-      console.log(likeButton);
-
-      console.log(postId);
-      try {
-
-          const response = await likePost(postId); // Call the function to like the post
-
-          // Update the likes count on success
-          const updatedLikesCount = response.likesCount; // Assuming the response contains the updated likes count
-          const likesCount = likeButton.parentElement.querySelector('.likes');
-          likesCount.textContent = updatedLikesCount;
-
-          console.log('Post liked successfully');
-          console.log(likesCount.textContent);
-      } catch (error) {
-          console.error('Error liking post:', error.message);
-          // Handle error condition
-      }
-  }
-});
 
 
-// Add an event listener to the like button
-document.addEventListener('DOMContentLoaded', function() {
-  const likeButton = document.querySelector('.like-button');
-  if (likeButton) {
-      likeButton.addEventListener('click', function(e) {
+
+// Event delegation for the like button inside the post container
+document.querySelector('.post-likes').addEventListener('click', async function(e) {
+    console.log(e.target);
+    if (e.target && e.target.matches('.like-button')) {
         e.preventDefault();
+  
+        const postId = e.target.getAttribute('data-id');
+        const likeButton = e.target; // Cache the like button element
+        console.log(likeButton);
+  
+        console.log(postId);
+        try {
+  
+            const response = await likePost(postId); // Call the function to like the post
+  
+            // Update the likes count on success
+            const updatedLikesCount = response.likesCount; // Assuming the response contains the updated likes count
+            const likesCount = likeButton.parentElement.querySelector('.likes');
+            likesCount.textContent = updatedLikesCount;
+  
+            console.log('Post liked successfully');
+            console.log(likesCount.textContent);
+        } catch (error) {
+            // nothing
 
-        const postId = this.getAttribute('data-id');
-        const likeButton = this; // Cache the like button element
-        const likesCount = document.querySelector('.likes'); // Select the element displaying the like count
-        console.log(likesCount.textContent);
-      
-        likePost(postId)
-            .then(response => {
-                console.log(response);
-                // Update the likes count on success
-                const updatedLikesCount = response.likesCount; // Assuming the response contains the updated likes count
-                likesCount.textContent = updatedLikesCount; // Update the like count in the DOM
-                console.log('Post liked successfully');
-                console.log(likesCount.textContent);
-            })
-            .catch(error => {
-                console.error('Error liking post:', error.message);
-                // Handle error condition
-            });
-      });
-  }
-});
+        }
+    }
+  });
+
+  // Event delegation for the like button inside the #timeline container
+document.querySelector('.comment').addEventListener('click', async function(e) {
+    console.log(e.target);
+    if (e.target && e.target.matches('.like-button')) {
+        e.preventDefault();
+  
+        const postId = e.target.getAttribute('data-id');
+        const likeButton = e.target; // Cache the like button element
+        console.log(likeButton);
+  
+        console.log(postId);
+        try {
+  
+            const response = await likePost(postId); // Call the function to like the post
+  
+            // Update the likes count on success
+            const updatedLikesCount = response.likesCount; // Assuming the response contains the updated likes count
+            const likesCount = likeButton.parentElement.querySelector('.likes');
+            likesCount.textContent = updatedLikesCount;
+  
+            console.log('Post liked successfully');
+        } catch (error) {
+            //console.error('Error liking post:', error.message);
+            // Handle error condition
+        }
+    }
+  });
 
 
+
+    // Event delegation for the like button inside the comments
+document.querySelector('#timeline').addEventListener('click', async function(e) {
+    console.log(e.target);
+    if (e.target && e.target.matches('.like-button')) {
+        e.preventDefault();
+  
+        const postId = e.target.getAttribute('data-id');
+        const likeButton = e.target; // Cache the like button element
+        console.log(likeButton);
+  
+        console.log(postId);
+        try {
+  
+            const response = await likePost(postId); // Call the function to like the post
+  
+            // Update the likes count on success
+            const updatedLikesCount = response.likesCount; // Assuming the response contains the updated likes count
+            const likesCount = likeButton.parentElement.querySelector('.likes');
+            likesCount.textContent = updatedLikesCount;
+  
+            console.log('Post liked successfully');
+            console.log(likesCount.textContent);
+        } catch (error) {
+            //console.error('Error liking post:', error.message);
+            // Handle error condition
+        }
+    }
+  });
+  
 
 
 
@@ -83,6 +114,6 @@ async function likePost(postId) {
       const responseData = await response.json();
       return { likesCount: responseData.likesCount }; // Update with the correct property from the backend response
   } catch (error) {
-      throw new Error(error.message);
+      //throw new Error(error.message);
   }
 }
