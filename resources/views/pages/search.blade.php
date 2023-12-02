2816da3c0746cd4a302881c720120db5a77db323
Félix Martins <felix.marcial@hotmail.com>
2023-12-02 22:12:39 +0000

    Changed search filters location in the page

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -9,36 +9,8 @@
             <h1 class="text-2xl font-bold">Search for users, groups, posts or comments</h1>
             @include('partials.header.search-bar')
         </header>
-        <section id="search-filters">
-            <header><h2>Search for:</h2></header>
-            <ul class="flex flex-col">
-                <li class="flex gap-2">
-                    <input type="radio" id="posts-radio" name="search-type" value="posts" checked>
-                    <label for="posts-radio">Posts</label>
-                </li>
-                <li class="flex gap-2">
-                    <input type="radio" id="comments-radio" name="search-type" value="comments">
-                    <label for="comments-radio">Comments</label>
-                </li>
-                <li class="flex gap-2">
-                    <input type="radio" id="users-radio" name="search-type" value="users">
-                    <label for="users-radio">Users</label>
-                </li>
-                <li class="flex gap-2">
-                    <input type="radio" id="groups-radio" name="search-type" value="groups">
-                    <label for="groups-radio">Groups</label>
-                </li>
-            </div>
-        </section>
         <div id="results-container"></div>
-        {{-- @if ($posts->count() == 0)
-            <p class="flex justify-center">No results found</p>
-        @else
-            @foreach($posts as $post)
-                @include('partials.post', ['post' => $post, 'displayComments' => false, 'showEdit' => false])
-            @endforeach
-        @endif --}}
     </section>
-    @include('partials.side.notifications-tab')
+    @include('partials.search.search_filters_side')
 </main>
 @endsection
---
+++ resources/views/partials/search/search_filters_side.blade.php
@@ -0,0 +1,25 @@
+<section class="col-span-1 flex content-center justify-center pl-16" id="notifications-tab">
+    <div class="h-full flex py-16 flex-col flex-grow">
+        <section id="search-filters">
+            <header><h2 class="text-xl">Search for:</h2></header>
+            <ul class="flex flex-col">
+                <li class="flex gap-2">
+                    <input type="radio" id="users-radio" name="search-type" value="users" checked>
+                    <label for="users-radio">Users</label>
+                </li>
+                <li class="flex gap-2">
+                    <input type="radio" id="groups-radio" name="search-type" value="groups">
+                    <label for="groups-radio">Groups</label>
+                </li>
+                <li class="flex gap-2">
+                    <input type="radio" id="posts-radio" name="search-type" value="posts">
+                    <label for="posts-radio">Posts</label>
+                </li>
+                <li class="flex gap-2">
+                    <input type="radio" id="comments-radio" name="search-type" value="comments">
+                    <label for="comments-radio">Comments</label>
+                </li>
+            </div>
+        </section>
+    </div>
+</section>
