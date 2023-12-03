f06dc8acb17b8df7faf9ab1a740756fd87886cf7
marco-vb <marcofilipe03@hotmail.com>
2023-12-03 22:58:39 +0000

    Fixed values: values were a bit off (90 comments per post, not realistic compared to likes).

--- resources/views/partials/post_info.blade.php
+++ resources/views/partials/post_info.blade.php
@@ -1,28 +1,30 @@
 @php
-    $editable = $showEdit && $post->isCreatedByCurrentUser();
+$editable = $showEdit && $post->isCreatedByCurrentUser();
 @endphp
 <div class="post-info">
     <header>
         <div class="user-date">
             <img class="user-image" src="{{ $post->createdBy->getProfilePicture() }}" alt="User photo">
-            <a class="post-info-user" href="/profile/{{ $post->createdBy->username }}">{{ $post->createdBy->username }}</a>
+            <a class="post-info-user" href="/profile/{{ $post->createdBy->username }}">{{ $post->createdBy->username
+                }}</a>
             <span class="date">{{ $post->created_at }}</span>
         </div>
         @if ($editable)
-            <div class="edit-delete-post">
-                <a href="#" class="edit edit-post">&#9998;</a>
-                <a href="{{ url('home') }}" class="delete delete-post">&#10761;</a>
-            </div>
+        <div class="edit-delete-post">
+            <a href="#" class="edit edit-post">&#9998;</a>
+            <a href="{{ url('home') }}" class="delete delete-post">&#10761;</a>
+        </div>
         @endif
     </header>
     <div class='post-body'>
         @if ($editable)
-            @include('partials.create_post_form', ['formClass' => 'edit-post-info hidden', 'textPlaceholder' => 'Edit post', 'contentValue' => $post->content, 'buttonText' => 'Update Post'])
+        @include('partials.create_post_form', ['formClass' => 'edit-post-info hidden', 'textPlaceholder' => 'Edit post',
+        'contentValue' => $post->content, 'buttonText' => 'Update Post'])
         @endif
         <a class="post-link" href="/post/{{ $post->id }}">
             <p class='post-content'>{{ $post->content }}</p>
-            @if ($post->media != null)
-                @include('partials.post_image', ['post' => $post, 'editable' => $editable])
+            @if ($post->media() != null)
+            @include('partials.post_image', ['post' => $post, 'editable' => $editable])
             @endif
         </a>
     </div>
