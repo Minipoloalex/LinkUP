5a1f00cccc210fddd497d19db2c6faab8fcce473
Duarte Gonçalves <up202108772@edu.fe.up.pt>
2023-12-03 21:52:13 +0000

    Replaced the edit profile pop-up modal with a designated section in the settings page

---
+++ public/js/settings.js
@@ -0,0 +1,37 @@
+const accountSettings = document.getElementById('account-settings');
+const accountSettingsToggle = document.getElementById('account-settings-toggle');
+
+const profileSettings = document.getElementById('profile-settings');
+const profileSettingsToggle = document.getElementById('profile-settings-toggle');
+
+const activeSection = document.getElementById('active-section');
+
+accountSettingsToggle.addEventListener('click', () => {
+    accountSettingsToggle.classList.add('border-b-2');
+    accountSettingsToggle.classList.add('border-black');
+    
+    profileSettingsToggle.classList.remove('border-b-2');
+    profileSettingsToggle.classList.remove('border-black');
+
+    // show the account settings
+    accountSettings.classList.remove('hidden');
+
+    // hide the profile settings
+    profileSettings.classList.add('hidden');
+});
+
+
+profileSettingsToggle.addEventListener('click', () => {
+    profileSettingsToggle.classList.add('border-b-2');
+    profileSettingsToggle.classList.add('border-black');
+
+    accountSettingsToggle.classList.remove('border-b-2');
+    accountSettingsToggle.classList.remove('border-black');
+
+    // show the profile settings
+    profileSettings.classList.remove('hidden');
+
+    // hide the account settings
+    accountSettings.classList.add('hidden');
+});
+
