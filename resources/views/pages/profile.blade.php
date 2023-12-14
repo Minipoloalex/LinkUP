155c336d3f12a0a090a22fb69b6359c7f89ff9ff
Félix Martins <felix.marcial@hotmail.com>
2023-12-14 09:31:04 +0000

    Fixed problem on user profile when user has no posts

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -7,14 +7,15 @@ Auth::user()->isFollowing($user)));
 @section('title', 'Profile')
 
 @section('content')
-<main id="profile-page" class=" flex flex-col w-screen
+<main id="profile-page" class="flex flex-col w-screen
                                 lg:w-full">
     <section class="flex flex-grow" id="content">
-        <section id="posts-container" data-id="{{ $user->id }}" data-page="0"
-            class=" overflow-clip overflow-y-scroll flex-grow h-full scrollbar-hide">
+        <section class="overflow-clip overflow-y-scroll flex-grow h-full scrollbar-hide">
             @include('partials.profile.user-banner')
             @if ($userCanSeePosts)
-            <div id="fetcher" class="h-16 lg:h-0"></div>
+                <div id="posts-container" data-id="{{ $user->id }}" data-page="0">
+                    <div id="fetcher" class="h-16 lg:h-0"></div>
+                </div>
             @else
             <p class="text-center">This user has a private profile.</p>
             @endif
