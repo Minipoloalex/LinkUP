c11518eee93adf671109c9d3812dcd485d213b06
marco-vb <marcofilipe03@hotmail.com>
2023-12-14 18:40:57 +0000

    Search bar in homepage now searches for users

--- resources/views/partials/side/right-tab.blade.php
+++ resources/views/partials/side/right-tab.blade.php
@@ -3,16 +3,21 @@
     <div class="hidden xl:flex xl:items-start xl:justify-start xl:w-3/4">
         <div class="w-full flex flex-col items-start justify-start gap-8">
             <div class="h-[5vh] w-full">
-                <form id="search-form" class="flex content-center justify-center py-1 rounded-full group"
+
+                <form id="search-home" class="flex content-center justify-center py-1 rounded-full group"
                     action="{{ url('/search') }}" method="GET">
+
                     <input id="search-text" class="align-middle border-b-2 dark:border-dark-secondary w-full dark:bg-dark-primary 
                 focus:outline-none group-focus-within:dark:border-dark-active" type="text" name="query">
+                    <input type="hidden" name="type" value="users">
                     <button
                         class="align-middle border-b-2 dark:border-dark-secondary group-focus-within:dark:border-dark-active text-2xl"
                         type="submit">
                         <i class="fas fa-search mr-2"></i>
                     </button>
+
                 </form>
+
             </div>
             <div class=" h-[30vh] w-full flex flex-col items-start justify-start pl-5 border rounded-md 
                     dark:border-dark-neutral z-10">
