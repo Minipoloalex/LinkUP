d9a0c4565c0979b3ac09039771f13a388edc0f46
marco-vb <marcofilipe03@hotmail.com>
2023-12-14 01:52:13 +0000

    Adapted notifications page to be responsive

--- resources/views/pages/notifications.blade.php
+++ resources/views/pages/notifications.blade.php
@@ -4,9 +4,8 @@
 
 @include('partials.side.navbar')
 
-<main id="homepage" class=" flex flex-col w-full overflow-clip overflow-y-scroll h-screen pt-36 scrollbar-hide
-                            md:pl-16
-                            lg:px-56">
+<main id="homepage" class=" flex flex-col w-screen overflow-clip overflow-y-scroll h-[calc(100vh-10rem)] scrollbar-hide
+                            lg:w-full">
 
     <h1 class="text-3xl font-bold text-center">Notifications</h1>
     <section class="flex flex-col w-full">
