4cbec24c78a330a4b62488d2d45285d5566ccffa
Félix Martins <felix.marcial@hotmail.com>
2023-12-17 12:00:19 +0000

    Made admin search be saveable/copiable

--- public/js/admin/admin_search_inf_scrolling.js
+++ public/js/admin/admin_search_inf_scrolling.js
@@ -1,9 +1,14 @@
 import { getSearchForm, getSearchTextElement } from '../search.js';
 import { infiniteScroll, destroyFetcher } from '../infinite_scrolling.js';
+import { getUrlParameter, setUrlParameters } from '../general_helpers.js';
+
+function getSearchQuery(searchForm) {
+    return {query: getSearchTextElement(searchForm).value};
+}
 
 export async function addInfiniteScrollToAdmin(container, testIntersectionElement, url, getElement, addEventListeners = null) {
     const searchForm = getSearchForm();
-    const query = {query: getSearchTextElement(searchForm).value};
+    const query = getSearchQuery(searchForm);
 
     const insert = async (data) => {
         for (const html of data.resultsHTML) {
@@ -32,8 +37,14 @@ export async function addEventListenersToSearchForm(container, testIntersectionE
     const searchForm = getSearchForm();
     const updateSearchResults = async (event) => {
         event.preventDefault();
+        setUrlParameters(getSearchQuery(searchForm));
         await addInfiniteScrollToAdmin(container, testIntersectionElement, url, getElement, null);
     }
     searchForm.addEventListener('submit', async (event) => await updateSearchResults(event));
     searchForm.addEventListener('input', async (event) => await updateSearchResults(event));
+}
+export async function initAdminSearch (url) {
+    const query = getUrlParameter('query');
+    const searchForm = getSearchForm();
+    getSearchTextElement(searchForm).value = query;
 }
