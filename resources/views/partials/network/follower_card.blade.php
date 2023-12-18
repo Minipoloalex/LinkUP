8eef75a534f5a0866adb48699efa1e504ffd635a
marco-vb <marcofilipe03@hotmail.com>
2023-12-18 16:13:20 +0000

    fixed bug in network, added descriptor, added create group button

--- resources/views/partials/network/follower_card.blade.php
+++ resources/views/partials/network/follower_card.blade.php
@@ -2,8 +2,8 @@
 $linkTo ??= route('profile.show', ['username' => $user->username]);
 @endphp
 @include('partials.network.follow_card', [
-    'user' => $user,
-    'isMyProfile' => $isMyProfile,
-    'buttons' => [['class' => 'delete-follower', 'text' => 'Remove']],
-    'linkTo' => $linkTo
+'user' => $user,
+'isMyProfile' => $isMyProfile,
+'buttons' => [['class' => 'delete-follower', 'text' => 'Remove']],
+'linkTo' => $linkTo
 ])
