5ea3e7b29502d4d8bfa8f329cf306f774f7e577d
marco-vb <marcofilipe03@hotmail.com>
2023-12-20 15:53:36 +0000

    fixed bug mark notifications as seen

--- app/Models/CommentNotification.php
+++ app/Models/CommentNotification.php
@@ -14,6 +14,7 @@ class CommentNotification extends Model
     protected $fillable = [
         'id_comment',
         'timestamp',
+        'seen',
     ];
     protected $dates = [
         'timestamp',
