0753ec413219ef82d1d42bc00bb127743effb2a8
marco-vb <marcofilipe03@hotmail.com>
2023-12-20 14:23:16 +0000

    aligned elements in the ui

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -13,13 +13,13 @@ $activePage = 'search';
                                 lg:w-full lg:h-[calc(100vh-6rem)]">
     <div class="dark:bg-dark-primary mt-4">
         <section id="search-content" class="flex gap-2">
-            <div class="w-full flex p-2">
-                <div class="w-full flex flex-col justify-center items-center">
-                    <form id="search-form" class="flex flex-col justify-center items-center py-1 rounded-full w-2/3"
+            <div class="w-full flex p-2 items-center justify-center">
+                <div class="w-2/3 flex flex-col justify-center items-center">
+                    <form id="search-form" class="flex flex-col justify-center items-center py-1 rounded-full w-full"
                         action="{{ url('/search') }}" method="GET">
                         <div class="w-full flex items-center justify-center">
                             <div
-                                class="rounded-full bg-transparent h-10 flex items-center justify-center border-2 border-dark-active">
+                                class="rounded-full bg-transparent h-10 flex items-center justify-center border-2 border-dark-active flex-grow">
                                 <button type="submit"><i class="fas fa-search text-white ml-3"></i></button>
                                 <input id="search-text" class="align-middle w-full bg-transparent ml-2 mr-3 text-white font-normal text-sm
                             focus:outline-none" type="text" name="query" placeholder="Search" autocomplete="off">
