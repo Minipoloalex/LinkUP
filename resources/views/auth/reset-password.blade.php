3aa518a2120d90c3ea05de253e9b65fb1f61e06a
Duarte Gonçalves <up202108772@edu.fe.up.pt>
2023-12-21 02:42:50 +0000

    Updated forgot and reset password forms

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -2,16 +2,16 @@
 
 @section('content')
 
-<main id="forgot-password" class="flex flex-col items-center justify-center w-full mt-24">
+<main id="forgot-password" class="flex flex-col items-center justify-center w-full mt-12">
     <div class="w-full max-w-md">
-        <div class="flex flex-col break-words bg-white border-2 rounded shadow-md">
+        <div class="flex flex-col break-words border-2 border-dark-active rounded-lg shadow-md p-8">
                 
-            <div class="font-bold bg-gray-200 text-gray-700 py-3 px-6">
+            <div class="font-bold py-3 px-6">
                 Reset Your Password
             </div>
             
             <div class="w-full p-6">
-                <p class="text-gray-700 text-sm">
+                <p class="text-sm">
                     Fill in the form below to reset your password.
                 </p>
             </div>
@@ -22,42 +22,42 @@
                 <input type="hidden" name="token" value="{{ $token }}">
 
                 <div class="flex flex-wrap mb-6">
-                    <label for="email" class="block text-gray-700 text-sm mb-2 font-semibold">
+                    <label for="email" class="block  text-sm mb-2 font-semibold">
                         Email Address
                         <span class="text-red-500 text-xs italic">*</span>
                     </label>
 
-                    <input id="email" type="email" class="form-input w-full{{ $errors->has('email') ? ' border-red-500' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus>
+                    <input id="email" type="email" class="form-input py-1 w-full bg-transparent outline-none border-b-2 border-dark-active {{ $errors->has('email') ? ' border-red-500' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus>
 
                 </div>
 
                 <div class="flex flex-wrap mb-6">
-                    <label for="password" class="block text-gray-700 text-sm mb-2 font-semibold">
+                    <label for="password" class="block text-sm mb-2 font-semibold">
                         Password
                         <span class="text-red-500 text-xs italic">*</span>
                     </label>
 
-                    <input id="password" type="password" class="form-input w-full{{ $errors->has('password') ? ' border-red-500' : '' }}" name="password" required>
+                    <input id="password" type="password" class="form-input py-1 w-full bg-transparent outline-none border-b-2 border-dark-active {{ $errors->has('password') ? ' border-red-500' : '' }}" name="password" required>
                 </div>
 
                 <div class="flex flex-wrap mb-6">
-                    <label for="password-confirm" class="block text-gray-700 text-sm mb-2 font-semibold">
+                    <label for="password-confirm" class="block text-sm mb-2 font-semibold">
                         Confirm Password
                         <span class="text-red-500 text-xs italic">*</span>
                     </label>
 
-                    <input id="password-confirm" type="password" class="form-input w-full" name="password_confirmation" required>
+                    <input id="password-confirm" type="password" class="form-input py-1 w-full bg-transparent outline-none border-b-2 border-dark-active" name="password_confirmation" required>
                 </div>
 
                 <div class="flex flex-wrap mt-16 mb-8">
-                    <button type="submit" class="bg-gray-200 hover:bg-gray-300 text-gray-700 font-bold py-2 px-4 rounded text-sm">
+                    <button type="submit" class="bg-dark-active font-bold py-2 px-4 rounded text-sm">
                         Reset Password   
                         <i class="fas fa-redo-alt fa-fw"></i>
                     </button>
                 </div>
 
                 <div class="flex flex-wrap">
-                    <a class="text-blue-500 hover:text-blue-700 text-sm" href="{{ route('login') }}">
+                    <a class="text-blue-400 hover:text-blue-500 text-sm" href="{{ route('login') }}">
                         <i class="fas fa-sign-in-alt fa-fw mr-1"></i>
                         Go to login
                     </a>
